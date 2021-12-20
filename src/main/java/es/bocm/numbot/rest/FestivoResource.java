3190061e82e1efd7066bb11d0959d9520efd5ca5
David del Rio Medina <ddrm86@gmail.com>
2021-12-20 12:50:38 +0100

    Add test for GET method.

--- src/main/java/es/bocm/numbot/rest/FestivoResource.java
+++ src/main/java/es/bocm/numbot/rest/FestivoResource.java
@@ -16,6 +16,7 @@ import java.lang.reflect.Type;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -44,11 +45,15 @@ public class FestivoResource {
             if (festivos.isEmpty()) {
                 return crearRespuestaFaltanFestivos();
             } else {
-                List<Map<String,String>> data = festivos.stream().map(Festivo::toMap).toList();
-                FestivoResponse response = new FestivoResponse(data);
-                return crearRespuestaJson(Response.Status.OK, response);
+                return crearRespuestaExitosa(festivos);
             }
         }
+    }
+
+    public static Response crearRespuestaExitosa(Collection<Festivo> extraordinarios) {
+        List<Map<String, String>> data = extraordinarios.stream().map(Festivo::toMap).toList();
+        FestivoResponse response = new FestivoResponse(data);
+        return crearRespuestaJson(Response.Status.OK, response);
     }
 
     @PUT
---
+++ src/test/java/es/bocm/numbot/rest/FestivoResourceTest.java
@@ -0,0 +1,94 @@
+package es.bocm.numbot.rest;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import es.bocm.numbot.entities.Festivo;
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.core.Application;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.jboss.resteasy.plugins.server.undertow.UndertowJaxrsServer;
+import org.jboss.resteasy.test.TestPortProvider;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FestivoResourceTest {
+    private static UndertowJaxrsServer server;
+
+    @ApplicationPath("/test")
+    public static class MyApp extends Application
+    {
+        @Override
+        public Set<Class<?>> getClasses()
+        {
+            HashSet<Class<?>> classes = new HashSet<>();
+            classes.add(FestivoResource.class);
+            return classes;
+        }
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        server = new UndertowJaxrsServer().start();
+        server.deployOldStyle(MyApp.class);
+    }
+
+    @AfterAll
+    static void afterAll() {
+        server.stop();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"asdf", "1", "12", "123", "123a"})
+    void producesCorrectInvalidYearResponse(String anno) {
+        String expected = "{\"exito\":false,\"data\":{\"error\":\"Año con formato incorrecto. " +
+                "El formato debe ser YYYY\"}}";
+        Client client = ClientBuilder.newClient();
+        Response response = client.target(TestPortProvider
+                .generateURL("/test/festivos/" + anno)).request(MediaType.APPLICATION_JSON).get();
+        assertEquals(Response.Status.BAD_REQUEST, response.getStatusInfo());
+        String json_res = response.readEntity(String.class);
+        assertEquals(expected, json_res);
+    }
+
+    @Test
+    void producesCorrectUnknownErrorGetResponse() {
+        String expected = "{\"exito\":false,\"data\":{\"error\":\"Error desconocido. con el administrador de sistemas" +
+                " para que revise la conexión con la BBDD y otras posibles causas.\"}}";
+        Client client = ClientBuilder.newClient();
+        // Will not be able to connect to the database
+        Response response = client.target(TestPortProvider
+                .generateURL("/test/festivos/1000")).request(MediaType.APPLICATION_JSON).get();
+        assertEquals(Response.Status.INTERNAL_SERVER_ERROR, response.getStatusInfo());
+        String json_res = response.readEntity(String.class);
+        assertEquals(expected, json_res);
+    }
+
+    @Test
+    void producesCorrectResponseWithData() {
+        List<Festivo> fests = List.of(
+                new Festivo(null, LocalDate.of(2021, 3, 1), "desc1"),
+                new Festivo(null, LocalDate.of(2021, 5, 14), "desc2")
+        );
+        String expected_str = "{\"exito\":true,\"data\":{\"festivos\":[{\"fecha\":\"03-01\"," +
+                "\"descripcion\":\"desc1\"},{\"fecha\":\"05-14\",\"descripcion\":\"desc2\"}]}}";
+        JsonObject expected = JsonParser.parseString(expected_str).getAsJsonObject();
+        Response response = FestivoResource.crearRespuestaExitosa(fests);
+        assertEquals(Response.Status.OK, response.getStatusInfo());
+        String json_res = response.readEntity(String.class);
+        JsonObject res = JsonParser.parseString(json_res).getAsJsonObject();
+        assertEquals(expected, res);
+    }
+}
