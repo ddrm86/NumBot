f3b0a5b157d05f0e489b1bae84b31108f1fddf9a
David del Rio Medina <ddrm86@gmail.com>
2021-12-22 13:56:35 +0100

    Add documentation for the calculations package.

--- src/main/java/es/bocm/numbot/calculations/CalcUtils.java
+++ src/main/java/es/bocm/numbot/calculations/CalcUtils.java
@@ -8,11 +8,22 @@ import java.time.Month;
 import java.util.Collection;
 import java.util.Set;
 
+/**
+ * Contiene métodos utilizados para cálculos de fechas.
+ */
 public final class CalcUtils {
     private CalcUtils() {
         throw new AssertionError("Clase de utilidades. No instanciar.");
     }
 
+    /**
+     * Calcula la fecha del domingo de Pascua para un año determinado.
+     *
+     * Se utiliza el algoritmo de Gauss.
+     *
+     * @param year el año para el que se calcula el domingo de Pascua.
+     * @return la fecha del domingo de Pascua para el año indicado.
+     */
     public static LocalDate getEasterSundayDate(int year) {
         int a = year % 19,
                 b = year / 100,
@@ -31,6 +42,14 @@ public final class CalcUtils {
         return LocalDate.of(year, month, day);
     }
 
+    /**
+     * Obtiene las tres fechas en que no se publica el boletín, aparte de los domingos, para un año determinado.
+     *
+     * Las fechas son el uno de enero, el veinticinco de diciembre y el Viernes Santo.
+     *
+     * @param anno año para el que se quieren obtener las fechas.
+     * @return las fechas en que no se publica el boletín ese año.
+     */
     public static Set<LocalDate> fechasSinBoletin(int anno) {
         final LocalDate unoEnero = LocalDate.of(anno, Month.JANUARY, 1);
         final LocalDate veinticincoDiciembre = LocalDate.of(anno, Month.DECEMBER, 25);
@@ -38,6 +57,18 @@ public final class CalcUtils {
         return Set.of(unoEnero, veinticincoDiciembre, viernesSanto);
     }
 
+    /**
+     * Calcula, a partir de una fecha determinada, el número de boletines seguidos que se publican en día no laboral.
+     *
+     * Estos boletines se suelen hacer antes en día laboral y se dejan aprobados para publicación.
+     * Por ejemplo, si estamos en viernes laboral y el lunes es día festivo con publicación de Boletín, debe devolver 2,
+     * ya que el sábado y el lunes no se trabaja, pero sí se publica el Boletín. El domingo no hay publicación y el
+     * martes se trabaja.
+     *
+     * @param fecha la fecha a partir de la cual se quieren calcular los días no laborales con Boletín seguidos.
+     * @param festivosAnno los festivos del año de la fecha indicada.
+     * @return número de boletines seguidos que se publican en día no laboral, a partir del día siguiente de la fecha.
+     */
     public static int numBotsEnFestivoSeguidos(LocalDate fecha, Collection<Festivo> festivosAnno) {
         int numBotsEnFestivoSeguidos = 0;
         boolean laboral = false;
@@ -57,10 +88,23 @@ public final class CalcUtils {
         return numBotsEnFestivoSeguidos;
     }
 
+    /**
+     * Comprueba si una fecha es un día sin publicación de Boletín, sin tener en cuenta los domingos.
+     *
+     * @param fecha la fecha que se quiere comprobar.
+     * @return true si es fecha sin boletín, false en caso contrario.
+     */
     public static boolean esFechaSinBoletin(LocalDate fecha) {
         return CalcUtils.fechasSinBoletin(fecha.getYear()).contains(fecha);
     }
 
+    /**
+     * Comprueba si una fecha es día festivo.
+     *
+     * @param fecha la fecha a comprobar.
+     * @param festivos los festivos para los que se quiere comprobar la fecha.
+     * @return true si la fecha corresponde a uno de los festivos proporcionados, false en caso contrario.
+     */
     private static boolean esFestivo(LocalDate fecha, Collection<Festivo> festivos) {
         return festivos.stream().anyMatch(f -> f.getFecha().equals(fecha));
     }
--- src/main/java/es/bocm/numbot/calculations/CalcularNumBot.java
+++ src/main/java/es/bocm/numbot/calculations/CalcularNumBot.java
@@ -8,11 +8,25 @@ import java.time.Month;
 import java.time.temporal.ChronoUnit;
 import java.util.Collection;
 
+/**
+ * Contiene los métodos necesarios para calcular el número de boletín que corresponde a una fecha.
+ */
 public final class CalcularNumBot {
     private CalcularNumBot() {
         throw new AssertionError("Clase de utilidades. No instanciar.");
     }
 
+    /**
+     * Calcula el número de Boletín que corresponde a una fecha determinada.
+     *
+     * Se tiene en cuenta el número de días del año que han pasado salvo los que no se publica el Boletín y el número
+     * de boletines extraordinarios que ha habido.
+     *
+     * @param fecha la fecha para la que se quiere obtener el número de Boletín.
+     * @param numExtMismoAnnoAntesOIgualFecha el número total de boletines extraordinarios que ha habido antes o en el
+     *                                        mismo día durante ese año.
+     * @return el número de Boletín.
+     */
     public static int getNumBot(LocalDate fecha, int numExtMismoAnnoAntesOIgualFecha) {
         final LocalDate unoEnero = LocalDate.of(fecha.getYear(), Month.JANUARY, 1);
         int numeroDias = (int) (ChronoUnit.DAYS.between(unoEnero, fecha) + 1);
@@ -24,6 +38,14 @@ public final class CalcularNumBot {
         return numeroDias + numExtMismoAnnoAntesOIgualFecha - calcNumDomingos(fecha) - numNoBot;
     }
 
+    /**
+     * Calcula el número de Boletín que corresponde a una fecha determinada.
+     *
+     * @param fecha la fecha para la que se quiere obtener el número de Boletín.
+     * @param extraordinariosAnno los boletines extraordinarios habidos durante el año. Se tendrán en cuenta solo los
+     *                            anteriores o iguales a la fecha.
+     * @return el número de Boletín.
+     */
     public static int getNumBot(LocalDate fecha, Collection<Extraordinario> extraordinariosAnno) {
         int numExtMismoAnnoAntesOIgualFecha = (int) extraordinariosAnno
                 .stream()
@@ -32,17 +54,35 @@ public final class CalcularNumBot {
         return getNumBot(fecha, numExtMismoAnnoAntesOIgualFecha);
     }
 
+    /**
+     * Calcula el número de días que han pasado del año hasta una fecha determinada.
+     *
+     * @param fecha la fecha para la que se quieren calcular los días.
+     * @return el número de días que han pasado desde el 1 de enero hasta la fecha, inclusive.
+     */
     private static int calcNumDias(LocalDate fecha) {
         final LocalDate unoEnero = LocalDate.of(fecha.getYear(), Month.JANUARY, 1);
         return (int) (ChronoUnit.DAYS.between(unoEnero, fecha) + 1);
     }
 
+    /**
+     * Calcula el número de domingos que han pasado del año hasta una fecha determinada.
+     *
+     * @param fecha la fecha para la que se quieren calcular los domingos.
+     * @return el número de domingos que han pasado desde el 1 de enero hasta la fecha.
+     */
     private static int calcNumDomingos(LocalDate fecha) {
         int numDias = calcNumDias(fecha);
         int diaPrimerDomingo = calcDiaPrimerDomingo(fecha);
         return (numDias + (7 - diaPrimerDomingo)) / 7;
     }
 
+    /**
+     * Calcula el número de días que han pasado hasta el primer domingo del año para una fecha determinada.
+     *
+     * @param fecha la fecha para la que se quiere calcular el primer domingo del año.
+     * @return el número de días que han pasado hasta el primer domingo del año, inclusive.
+     */
     private static int calcDiaPrimerDomingo(LocalDate fecha) {
         LocalDate fechaAux = LocalDate.of(fecha.getYear(), Month.JANUARY, 1);
         while (fechaAux.getDayOfWeek() != DayOfWeek.SUNDAY) {
