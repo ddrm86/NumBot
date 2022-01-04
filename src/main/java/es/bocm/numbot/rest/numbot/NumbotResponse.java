e3da3f2f3d6cd81358ffdd497316d10d5f469313
David del Río Medina <ddrm86@gmail.com>
2022-01-04 09:51:40 +0100

    Add logging for NumbotResponse.

--- src/main/java/es/bocm/numbot/rest/numbot/NumbotResponse.java
+++ src/main/java/es/bocm/numbot/rest/numbot/NumbotResponse.java
@@ -1,6 +1,8 @@
 package es.bocm.numbot.rest.numbot;
 
 import es.bocm.numbot.rest.NumbotApiResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
@@ -8,6 +10,8 @@ import java.util.Map;
  * Respuesta exitosa para el recurso de numero-boletin.
  */
 public record NumbotResponse(boolean exito, Map<String, String> data) implements NumbotApiResponse {
+    private static final Logger log = LoggerFactory.getLogger(NumbotResponse.class);
+
     /**
      * Crea una respuesta a partir de los datos.
      *
@@ -17,5 +21,7 @@ public record NumbotResponse(boolean exito, Map<String, String> data) implements
     public NumbotResponse(String numBot, String numBotsEnFestivoSeguidos) {
         this(true, Map.of("numero_boletin", numBot,
                 "numero_boletines_en_no_laboral_seguidos", numBotsEnFestivoSeguidos));
+        log.debug("Creado NumbotResponse con número de boletín {} y bots en no laborales seguidos {}",
+                numBot, numBotsEnFestivoSeguidos);
     }
 }
