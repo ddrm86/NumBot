e8d70ca44a932a0989eab9bbee5eb89b7659bc1e
David del Río Medina <ddrm86@gmail.com>
2022-01-04 10:09:48 +0100

    Add logging for Extraordinario resource.

--- src/main/java/es/bocm/numbot/rest/extraordinarios/ExtraordinarioResponse.java
+++ src/main/java/es/bocm/numbot/rest/extraordinarios/ExtraordinarioResponse.java
@@ -1,6 +1,8 @@
 package es.bocm.numbot.rest.extraordinarios;
 
 import es.bocm.numbot.rest.NumbotApiResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Map;
@@ -9,6 +11,8 @@ import java.util.Map;
  * Respuesta exitosa para el recurso de boletines extraordinarios.
  */
 public record ExtraordinarioResponse(boolean exito, Map<String, List<Map<String, String>>> data) implements NumbotApiResponse {
+    private static final Logger log = LoggerFactory.getLogger(ExtraordinarioResponse.class);
+
     /**
      * Crea una respuesta a partir de los datos.
      *
@@ -16,5 +20,6 @@ public record ExtraordinarioResponse(boolean exito, Map<String, List<Map<String,
      */
     public ExtraordinarioResponse(List<Map<String, String>> data) {
         this(true, Map.of("extraordinarios", data));
+        log.debug("Creando ExtraordinarioResponse con los datos {}", data);
     }
 }
